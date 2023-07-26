8ed4e5f7bc04be5eb830ddff083f5b05120fa940
Anthony O <antman@ANTMANS-Air.lan>
2023-07-26 10:47:03 -0400

    random facts

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,11 @@ function App() {
 
   return (
     <>
-      <div>
+     <div>
+      Ant man is learning code
+     </div>
+     
+      {/* /* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +31,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */} 
     </>
   )
 }
